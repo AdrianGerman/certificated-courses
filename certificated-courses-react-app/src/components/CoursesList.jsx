376c842567f4d9492ee29d93d79c90d4097bd502
AdrianGerman <adrian.german1019@gmail.com>
2024-08-20 12:49:21 -0600

    separate the data

--- certificated-courses-react-app/src/components/CoursesList.jsx
+++ certificated-courses-react-app/src/components/CoursesList.jsx
@@ -1,32 +1,6 @@
 import "./CoursesContent.css"
 import CourseCard from "./CourseCard"
-
-const courses = [
-  {
-    title: "Curso 1",
-    description: "Descripción del curso 1",
-    image:
-      "https://pbs.twimg.com/profile_images/1776312145816227840/ISlE225__400x400.jpg"
-  },
-  {
-    title: "Curso 2",
-    description: "Descripción del curso 2",
-    image:
-      "https://pbs.twimg.com/profile_images/1776312145816227840/ISlE225__400x400.jpg"
-  },
-  {
-    title: "Curso 2",
-    description: "Descripción del curso 2",
-    image:
-      "https://pbs.twimg.com/profile_images/1776312145816227840/ISlE225__400x400.jpg"
-  },
-  {
-    title: "Curso 2",
-    description: "Descripción del curso 2",
-    image:
-      "https://pbs.twimg.com/profile_images/1776312145816227840/ISlE225__400x400.jpg"
-  }
-]
+import courses from "../data/courses"
 
 export const CoursesList = () => {
   return (
---
+++ certificated-courses-react-app/src/data/courses.js
@@ -0,0 +1,28 @@
+const courses = [
+  {
+    title: "Curso 1",
+    description: "Descripción del curso 1",
+    image:
+      "https://pbs.twimg.com/profile_images/1776312145816227840/ISlE225__400x400.jpg"
+  },
+  {
+    title: "Curso 2",
+    description: "Descripción del curso 2",
+    image:
+      "https://pbs.twimg.com/profile_images/1776312145816227840/ISlE225__400x400.jpg"
+  },
+  {
+    title: "Curso 2",
+    description: "Descripción del curso 2",
+    image:
+      "https://pbs.twimg.com/profile_images/1776312145816227840/ISlE225__400x400.jpg"
+  },
+  {
+    title: "Curso 2",
+    description: "Descripción del curso 2",
+    image:
+      "https://pbs.twimg.com/profile_images/1776312145816227840/ISlE225__400x400.jpg"
+  }
+]
+
+export default courses
