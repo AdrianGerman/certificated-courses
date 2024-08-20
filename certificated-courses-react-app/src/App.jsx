0ec95a7cf82b5c9f3a8ca9e68a4714dc45b2971e
AdrianGerman <adrian.german1019@gmail.com>
2024-08-20 11:54:54 -0600

    section courses

--- certificated-courses-react-app/src/App.jsx
+++ certificated-courses-react-app/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css"
+import { CoursesContent } from "./components/CoursesContent"
 import { HeaderTitle } from "./components/HeaderTitle"
 
 function App() {
   return (
     <main>
       <HeaderTitle />
+      <CoursesContent />
     </main>
   )
 }
