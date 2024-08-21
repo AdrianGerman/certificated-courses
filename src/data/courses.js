b9a8046f57a4e686db3c8b12769d519b47f72c93
AdrianGerman <adrian.german1019@gmail.com>
2024-08-21 08:47:26 -0600

    data update

--- src/data/courses.js
+++ src/data/courses.js
@@ -1,27 +1,45 @@
 const courses = [
   {
-    title: "Curso 1",
-    description: "Descripción del curso 1",
-    image:
-      "https://pbs.twimg.com/profile_images/1776312145816227840/ISlE225__400x400.jpg"
+    title: "Fundamentos de JavaScript",
+    description:
+      "Domina las bases de JavaScript, manipula el DOM y mejora la interactividad de tus proyectos. Utiliza Web APIs, escribe código seguro y escalable con TypeScript.",
+    image: ""
   },
   {
-    title: "Curso 2",
-    description: "Descripción del curso 2",
-    image:
-      "https://pbs.twimg.com/profile_images/1776312145816227840/ISlE225__400x400.jpg"
+    title: "Python: De Cero a Experto",
+    description:
+      "Explora la programación con cursos de Python, de cero a frameworks avanzados. Construye proyectos backend y domina el lenguaje más usado.",
+    image: "../../public/courses-rutes/diploma-web-python.jpg"
   },
   {
-    title: "Curso 2",
-    description: "Descripción del curso 2",
-    image:
-      "https://pbs.twimg.com/profile_images/1776312145816227840/ISlE225__400x400.jpg"
+    title: "HTML y CSS desde cero",
+    description:
+      "Aprende a hacer páginas eficientes, maquetación con HTML, estilos con CSS más complejos y con animaciones.",
+    image: "../../public/courses-rutes/diploma-web-frontend.jpg"
   },
   {
-    title: "Curso 2",
-    description: "Descripción del curso 2",
-    image:
-      "https://pbs.twimg.com/profile_images/1776312145816227840/ISlE225__400x400.jpg"
+    title: "Frontend a profundidad con Vue.js",
+    description:
+      "Vue.js es un primer gran framework frontend para aprender gracias a su suave curva de aprendizaje y fuerte presencia en la comunidad de JavaScript.",
+    image: "../../public/courses-rutes/diploma-vue.jpg"
+  },
+  {
+    title: "Frontend con Vue.js",
+    description:
+      "Vue.js es un primer gran framework frontend para aprender gracias a su suave curva de aprendizaje y fuerte presencia en la comunidad de JavaScript.",
+    image: "../../public/courses-rutes/diploma-web-vue.jpg"
+  },
+  {
+    title: "Fundamentos de Programación y Desarrollo Web",
+    description:
+      "Conoce más sobre computación básica y pensamiento lógico y programación orientada a desarrollo web.",
+    image: "../../public/courses-rutes/diploma-web-fundamentos.jpg"
+  },
+  {
+    title: "Frontend con React.js",
+    description:
+      "Especialízate en React.js, la herramienta más demandada y en mayor crecimiento de JavaScript para desarrollo frontend.",
+    image: "../../public/courses-rutes/diploma-web-react.jpg"
   }
 ]
 
